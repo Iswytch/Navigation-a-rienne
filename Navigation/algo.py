68b7742ee3f823ba89b9440f91086961e5ed46bd
Antoine DANNA <danna.antoine@gmail.com>
2020-11-19 11:01:00 +0100

    Update algo.py

--- Navigation/algo.py
+++ Navigation/algo.py
@@ -22,10 +22,12 @@ def calculDistanceLoxo(latPt, longPt, latPtArrive, longPtArrive):
 
 def calculCap(latM, longM, latN, longN, distance, estOuOuest):
     V=(sin(latN)-sin(latM)*cos(distance))/(cos(latM)*sin(distance))
-    if estOuOuest=="e":
+    if estOuOuest(longPt,longPtArrive)=="e":
         return acos(V)
-    else:
+    elif estOuOuest(longPt,longPtArrive)=="o":
         return 2*pi*(-acos(V))
+    else:
+        exit()
     
     
 def latNextPoint(latM, longM, capInitial, l):
@@ -35,28 +37,37 @@ def latNextPoint(latM, longM, capInitial, l):
 def longNextPoint(latM, longM, capInitial, l):
     Q = pi/(1.852*60*180)
     return longM+sin(cap)/cos(latM)*l*Q
+
+
+def estOuOuest(longPt,longPtArrive):
+    if(longPt<longPtArrive):
+        print("on va de l'ouest vers l'est")
+        return "e"
+    else:
+        print("on va de l'est vers l'ouest")
+        return "o"
+        
     
 
 im = plt.imread("equi.jpg")
 
 #Point de départ
-#latM = float(input("Entrez la latitude du point M : "))
-#longM = float(input("Entrez la longitude du point M : "))
+#latPt = float(input("Entrez la latitude du point de départ : "))
+#longPt = float(input("Entrez la longitude du point de départ : "))
 
 #Point d'arrivé
-#latN=float(input("Entrez la latitude du point N : "));
-#longN=float(input("Entrez la longitude du point N : "));
+#latPtArrive=float(input("Entrez la latitude du point d'arrivé : "));
+#longPtArrive=float(input("Entrez la longitude du point d'arrivé : "));
 
-#estOuOuest=input("Entrez o si vous vous déplacez de l'est vers l'ouest ou e de l'ouest vers l'est : ")
 
 #Distance à parcourir entre les 2 points
 
 l = 100
-latPt = 48.85
-longPt = 2.72
+latPt = 48.8534
+longPt =  2.3488
 latPtArrive = 35.689487
 longPtArrive = 139.691706
-estOuOuest="e"
+
 
 distanceOrtho = 0
 i=0
@@ -92,14 +103,20 @@ longPtSuivant=longNextPoint(latPt, longPt, cap, l)
 latPtSuivant=latNextPoint(latPt, longPt, cap, l)      
 
 
-
 check=True
 
 while check:
     
-     if (round(latPt,1)==round(latPtArrive,1) and round(longPt,1)==round(longPtArrive,1)):
+     print("latPt = ",(round(radiantToDegre(latPt),1)))
+     print("longPt = ",(round(radiantToDegre(longPt),1)))
+     print("latPtArrive = ",(round(radiantToDegre(latPtArrive),1)))
+     print("longPtArrive = ",(round(radiantToDegre(longPtArrive),1)))
+           
+     #TODO changer et mettre des degre?
+     if (round(latPt,1)==round(latPtArrive,1) and round(longPt,1)==round(longPtArrive,1)or i>1000):
         check=False
-        
+           
+     i=i+1;
     
      coordXCarte=512.5-((512.5*radiantToDegre(latPt))/90)
      coordYCarte=1024+((1024*radiantToDegre(longPt))/180)
@@ -119,14 +136,6 @@ while check:
      
      cap=calculCap(latPt, longPt, latPtArrive, longPtArrive, distance, estOuOuest)
     
-
-    
-    
-     # DEBUG
-     # print("longPt = ",longPt)
-     # print("longPtArrive = ",longPtArrive)
-     # print("latPt = ",latPt)
-     # print("latPtArrive = ",latPtArrive)
 
     
      
