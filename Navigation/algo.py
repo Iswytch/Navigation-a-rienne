15e960e48ce05c5089622a59b8d56391c2bfeea1
Antoine DANNA <danna.antoine@gmail.com>
2020-11-21 18:27:11 +0100

    Tour du monde par est ou ouest

--- Navigation/algo.py
+++ Navigation/algo.py
@@ -25,7 +25,7 @@ def calculCap(latM, longM, latN, longN, distance, estOuOuest):
     if estOuOuest(longPt,longPtArrive)=="e":
         return acos(V)
     elif estOuOuest(longPt,longPtArrive)=="o":
-        return 2*pi*(-acos(V))
+        return 2*pi-acos(V)
     else:
         exit()
     
@@ -40,35 +40,37 @@ def longNextPoint(latM, longM, capInitial, l):
 
 
 def estOuOuest(longPt,longPtArrive):
-    if(longPt<longPtArrive):
-        print("on va de l'ouest vers l'est")
-        return "e"
+    if calculDistanceLoxo(latPt, longPt, latPtArrive, longPtArrive) > 12500:
+        if(longPt<longPtArrive):
+            return "o"
+        else:
+            return "e"
     else:
-        print("on va de l'est vers l'ouest")
-        return "o"
+        if(longPt<longPtArrive):
+            return "e"
+        else:
+            return "o"
         
     
 
 im = plt.imread("equi.jpg")
 
 #Point de départ
-#latPt = float(input("Entrez la latitude du point de départ : "))
-#longPt = float(input("Entrez la longitude du point de départ : "))
+# latPt = float(input("Entrez la latitude du point de départ : "))
+# longPt = float(input("Entrez la longitude du point de départ : "))
 
-#Point d'arrivé
-#latPtArrive=float(input("Entrez la latitude du point d'arrivé : "));
-#longPtArrive=float(input("Entrez la longitude du point d'arrivé : "));
+# #Point d'arrivé
+# latPtArrive=float(input("Entrez la latitude du point d'arrivé : "));
+# longPtArrive=float(input("Entrez la longitude du point d'arrivé : "));
 
 
 #Distance à parcourir entre les 2 points
 
 l = 100
-latPt = 48.8534
-longPt =  2.3488
-latPtArrive = 35.689487
-longPtArrive = 139.691706
-
-
+latPt= 40.8822860825291
+longPt= -73.71750689359394
+latPtArrive= 35.70503419924903
+longPtArrive=139.80218267578786
 distanceOrtho = 0
 i=0
 listeLat = []
@@ -86,6 +88,8 @@ longPtArrive = degreToRadiant(longPtArrive)
 # Distance loxodromique
 
 distanceLoxo = calculDistanceLoxo(latPt, longPt, latPtArrive, longPtArrive)
+print("")
+print("")
 print("Distance loxodromique = ",distanceLoxo)
 
 
@@ -97,23 +101,30 @@ distance=calculDistanceOrtho(latPt, longPt, latPtArrive, longPtArrive)
 cap=calculCap(latPt, longPt, latPtArrive, longPtArrive, distance, estOuOuest)
 
 
+
+
 print("Cap initial = ",(radiantToDegre(cap))%360) 
+
+
+
+#Boucle permettant de calculer la distance orthodromique
 
 longPtSuivant=longNextPoint(latPt, longPt, cap, l)
 latPtSuivant=latNextPoint(latPt, longPt, cap, l)      
-
-
 check=True
+
 
 while check:
     
      print("latPt = ",(round(radiantToDegre(latPt),1)))
      print("longPt = ",(round(radiantToDegre(longPt),1)))
+     print("latPtSuivant = ",(round(radiantToDegre(latPtSuivant),1)))
+     print("longPtSuivant = ",(round(radiantToDegre(longPtSuivant),1)))
      print("latPtArrive = ",(round(radiantToDegre(latPtArrive),1)))
      print("longPtArrive = ",(round(radiantToDegre(longPtArrive),1)))
+     print("--------------------------------------------------")
            
-     #TODO changer et mettre des degre?
-     if (round(latPt,1)==round(latPtArrive,1) and round(longPt,1)==round(longPtArrive,1)or i>1000):
+     if (round(latPt,1)==round(latPtArrive,1) and round(longPt,1)==round(longPtArrive,1)or i>150):
         check=False
            
      i=i+1;
@@ -126,8 +137,35 @@ while check:
 
      distanceOrtho=distanceOrtho+calculDistanceOrtho(latPt, longPt, latPtSuivant, longPtSuivant)
     
-     latPt=latPtSuivant
-     longPt=longPtSuivant
+    
+    
+    
+     #Check si il faut faire le tour de la carte et le fait si il faut
+     
+     if(latPtSuivant>degreToRadiant(90)):
+         latPtSuivant=latPtSuivant-degreToRadiant(180)
+     else:
+         latPt=latPtSuivant
+         
+     if(latPtSuivant<degreToRadiant(-90)):
+         latPtSuivant=latPtSuivant+degreToRadiant(180)
+     else:
+         latPt=latPtSuivant
+
+     
+     if(longPtSuivant>degreToRadiant(180)):
+         longPtSuivant=longPtSuivant-degreToRadiant(360)
+     else:
+         longPt=longPtSuivant
+         
+     if(longPtSuivant<degreToRadiant(-180)):
+         longPtSuivant=longPtSuivant+degreToRadiant(360)
+     else:
+         longPt=longPtSuivant
+   
+    
+   
+    
    
      longPtSuivant=longNextPoint(latPtSuivant, longPtSuivant, cap, l)
      latPtSuivant=latNextPoint(latPtSuivant, longPtSuivant, cap, l)
